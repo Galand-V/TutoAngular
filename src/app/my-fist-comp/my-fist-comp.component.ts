68de5efc62434678b248aabda6c6ee0dec940877
unknown <galandvictor@gmail.com>
2018-12-17 20:01:54 +0100

    new modif for the tuto

---
+++ src/app/my-fist-comp/my-fist-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-fist-comp',
+  templateUrl: './my-fist-comp.component.html',
+  styleUrls: ['./my-fist-comp.component.scss']
+})
+export class MyFistCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
